aefb64a7adf4e5b25c8a4f86853fb0443436c660
LordNuker08 <lordnuker08@gmail.com>
2019-06-17 15:52:52 +0100

    Restructure

---
+++ scripts/src/event-manager.js
@@ -0,0 +1,25 @@
+export default function EventManager(options) {
+  var jq = options.jq;
+  var NAMESPACE = ".ln08"; // :)
+  var EVENT_LOAD_CHARACTER_DATA = "load_character_data" + NAMESPACE;
+  var EVENT_REDRAW_CHARTS = "redraw_chars" + NAMESPACE;
+
+  this.triggerLoadCharacterData = function(eventData) {
+    jq(document).trigger(EVENT_LOAD_CHARACTER_DATA, {
+      characterIndex: eventData.characterIndex,
+      membershipInfoIndex: eventData.membershipInfoIndex
+    });
+  };
+
+  this.triggerRedrawCharts = function(eventData) {
+    jq(document).trigger(EVENT_REDRAW_CHARTS, eventData);
+  };
+
+  this.getCharacterDataLoadedEventName = function() {
+    return EVENT_LOAD_CHARACTER_DATA;
+  };
+
+  this.getRedrawChartsEventName = function() {
+    return EVENT_REDRAW_CHARTS;
+  };
+}
